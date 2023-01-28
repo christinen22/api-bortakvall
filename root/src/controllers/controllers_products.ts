2f31c7d823eda889d4a519db645e42f2d3cba070
christinen22 <christine.elin.louise@gmail.com>
2023-01-28 12:38:01 +0100

    Post and get products

--- root/src/controllers/controllers_products.ts
+++ root/src/controllers/controllers_products.ts
@@ -1,30 +1,32 @@
-/**
- * Controller Template
- */
 import { Request, Response } from 'express'
+import Debug from 'debug'
 import prisma from '../prisma'
 
+// Create a new debug instance
+const debug = Debug('prisma-products:controllers_products')
 
+//
 // Get all products
-
+//
 export const index = async (req: Request, res: Response) => {
-    {
-        try {
-            const product = await prisma.products.findMany()
-            res.send(product)
-    
-        } catch (err) {
-            console.error(err)
-            res.status(500).send({
-                message: "Something went wrong querying the database.",
-            })
-        }
-    }
+	try {
+		const products = await prisma.products.findMany()
+
+		res.send({
+			status: "success",
+			data: products,
+		})
+
+	} catch (err) {
+		debug("Error thrown when finding products", err)
+		res.status(500).send({ status: "error", message: "Something went wrong" })
+	}
 
 }
 
-
+//
 // Get a single product
+//
 export const show = async (req: Request, res: Response) => {
     const productId = Number(req.params.productId)
 
@@ -36,7 +38,10 @@ export const show = async (req: Request, res: Response) => {
 
 		})
 
-		res.send(product)
+		res.send({
+			status: "success",
+			data: product,
+		})
 
 	} catch (err) {
 		console.error(err)
@@ -46,24 +51,34 @@ export const show = async (req: Request, res: Response) => {
 	}
 }
 
-/**
- * Create a resource
- */
+//
+//Create product
+//
 export const store = async (req: Request, res: Response) => {
-    try {
+	try {
 		const product = await prisma.products.create({
-			data: req.body,
+			data: {
+				name: req.body.name,
+				description: req.body.description,
+				price: req.body.price,
+				on_sale: req.body.on_sale,
+				images: req.body.images,
+				stock_status: req.body.stock_status,
+				stock_quantity: req.body.stock_quantity,
+			}
 		})
 
-		res.status(201).json(product)
+		res.send({
+			status: "success",
+			data: product,
+		})
 
 	} catch (err) {
-		console.error(err)
-		res.status(500).send({
-			message: "Something went wrong creating the record in the database.",
-		})
+		debug("Error thrown when creating a product %o: %o", req.body, err)
+		res.status(500).send({ status: "error", message: "Something went wrong" })
 	}
 }
+
 
 /**
  * Update a resource
