bb9e409c550ad7f3575dc1c834ea900e1819fa8b
christinen22 <christine.elin.louise@gmail.com>
2023-01-28 13:01:42 +0100

    Routes and controllers for orders

--- root/src/controllers/controllers_orders.ts
+++ root/src/controllers/controllers_orders.ts
@@ -1,9 +1,9 @@
-/**
- * Controller Template
- */
 import { Request, Response } from 'express'
+import Debug from 'debug'
 import prisma from '../prisma'
 
+// Create a new debug instance
+const debug = Debug('prisma-products:controllers_products')
 
 // Get all orders
 export const index = async (req: Request, res: Response) => {
@@ -35,7 +35,10 @@ export const show = async (req: Request, res: Response) => {
 
 		})
 
-		res.send(order)
+		res.send({
+			status: "success",
+			data: order,
+		})
 
 	} catch (err) {
 		console.error(err)
@@ -45,20 +48,38 @@ export const show = async (req: Request, res: Response) => {
 	}
 }
 
-/**
- * Create a resource
- */
+//
+//Create an order
+//
 export const store = async (req: Request, res: Response) => {
+	try {
+		const order = await prisma.orders.create({
+			data: {
+				order_date: req.body.order_date,
+				customer_first_name: req.body.customer_first_name,  
+				customer_last_name: req.body.customer_last_name,
+				customer_address: req.body.customer_address,    
+			  	customer_postcode: req.body.customer_postcode,   
+				customer_city: req.body.customer_city,       
+				customer_email: req.body.customer_email,      
+				customer_phone: req.body.customer_phone,      
+				order_total: req.body.order_total,         
+				created_at: req.body.created_at,          
+				updated_at: req.body.updated_at,         
+				items: req.body.items 
+			
+			}
+		})
+
+		res.send({
+			status: "success",
+			data: order,
+		})
+
+	} catch (err) {
+		debug("Error thrown when creating a order %o: %o", req.body, err)
+		res.status(500).send({ status: "error", message: "Something went wrong" })
+	}
+
 }
 
-/**
- * Update a resource
- */
-export const update = async (req: Request, res: Response) => {
-}
-
-/**
- * Delete a resource
- */
-export const destroy = async (req: Request, res: Response) => {
-}
--- root/src/routes/routes_orders.ts
+++ root/src/routes/routes_orders.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { body } from 'express-validator'
-import { index, show, store, update, destroy } from '../controllers/controllers_orders'
+import { index, show, store } from '../controllers/controllers_orders'
 const router = express.Router()
 
 // Get all orders
@@ -12,10 +12,6 @@ router.get('/:orderId', show)
 // Post orders
 router.post('/orders', store)
 
-// Update order
-router.patch('/:orderId', update)
 
-// Delete order
-router.delete('/:orderId', destroy)
 
 export default router
